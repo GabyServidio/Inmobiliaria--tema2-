e4a6924accf47b022c0a8944559f1d6ca6d10995
GabyServidio <gabrielserv@hotmail.com.ar>
2023-10-03 12:58:49 -0300

    Se craron las clases entidades y se completó la clase Persona con los 3 costructores y getters y setters. Clase inmueble quedamos en los atributos

---
+++ Inmobiliaria/src/Entidades/Inmueble.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Inmueble {
+    private int id;
+    private int idPropietario;
+    private int superficie;
+    private int cantAmbientes;
+    private int canBaños;
+    private LocalDate fechaConstruccion;
+    private int garage;
+    private String direccion;
+    private String zona;
+    private String tipo;
+    private String estadoInmueble;
+    
+    
+    
+}
---
+++ Inmobiliaria/src/Entidades/Persona.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected int dni;
+    protected int cuil;
+    protected String domicilio;
+    protected int telefono;
+    protected String email;
+    protected boolean estado;
+
+    public Persona(int id, String nombre, String apellido, int dni, int cuil, String domicilio, int telefono, String email, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.cuil = cuil;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.email = email;
+        this.estado = estado;
+    }
+
+    public Persona(String nombre, String apellido, int dni, int cuil, String domicilio, int telefono, String email, boolean estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.cuil = cuil;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.email = email;
+        this.estado = estado;
+    }
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getCuil() {
+        return cuil;
+    }
+
+    public void setCuil(int cuil) {
+        this.cuil = cuil;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
