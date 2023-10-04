3294eb92a4e7990c945a010cfee25eb4a31ddb74
GabyServidio <gabrielserv@hotmail.com.ar>
2023-10-04 11:53:37 -0300

    clase de conexion configurada y creacion de archivo conexion.env que permite configurar individualmente la conexion.

--- Inmobiliaria/src/AccesoADatos/Conexion.java
+++ Inmobiliaria/src/AccesoADatos/Conexion.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoADatos;
 
-/**
- *
- * @author USUARIO
- */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+
+    private static String URL;
+    private static String USER ;
+    private static String PASS ;
+         
+    public static Connection getConexion(){
+        Connection conexion = null;
+        recuperar();
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "ERROR DE CONEXION CON EL SERVIDOR");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR DE DRIVER");
+        }
+    return conexion;
+    }
+    public static void recuperar(){
+    try{
+    FileReader direccion = new FileReader("src/AccesoADatos/conexion.env");
+    BufferedReader lector = new BufferedReader(direccion);
+    Properties propiedades = new Properties();
+    propiedades.load(lector);
+    URL = propiedades.getProperty("DB_URL");
+    USER = propiedades.getProperty("DB_USER");
+    PASS = propiedades.getProperty("DB_PASS");
+        System.out.println(URL);
     
+    }   catch (FileNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{}
+    
+    }
 }
--- Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
+++ Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
@@ -4,6 +4,8 @@
  */
 package inmobiliaria;
 
+import AccesoADatos.Conexion;
+
 /**
  *
  * @author USUARIO
@@ -14,7 +16,7 @@ public class Inmobiliaria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+Conexion.getConexion();    
     }
     
 }
