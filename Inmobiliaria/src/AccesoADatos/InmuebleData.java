ecaf461c8c5d14bf413006a2bf477028bd5ad691
Ironwall72 <134495742+Ironwall72@users.noreply.github.com>
2023-10-05 19:46:58 -0300

    actualización inmuebledata.java

--- Inmobiliaria/src/AccesoADatos/InmuebleData.java
+++ Inmobiliaria/src/AccesoADatos/InmuebleData.java
@@ -8,6 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -53,5 +55,87 @@ public class InmuebleData {
             }
         }
     }
+    
+    public Inmueble buscarInmueble(int idPropietario) {
+        Inmueble inmueble = null;
+        String sql = "SELECT superficie, cantAmbientes, canBaños, fechaConstruccion,"
+                + " garage, estadoInmueble, direccion, zona, condicionesContrato"
+                + " FROM inmueble WHERE idPropietario = ?";
+        ps = null;
+        try {
+            ps = Conexion.getConexion().prepareStatement(sql);
+            ps.setInt(1, idPropietario);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                inmueble = new Inmueble();
+                inmueble.setId(rs.getInt("id"));
+                inmueble.setIdPropietario(rs.getInt(idPropietario));
+                inmueble.setSuperficie(rs.getInt("superficie"));
+                inmueble.setCantAmbientes(rs.getInt("cantAmbientes"));
+                inmueble.setCanBaños(rs.getInt("canBaños"));
+                inmueble.setFechaConstruccion(rs.getDate("fechaConstruccion").toLocalDate());
+                inmueble.setGarage(rs.getInt("garage"));
+                inmueble.setEstadoInmueble(rs.getString("estadoInmueble"));
+                inmueble.setDireccion(rs.getString("direccion"));
+                inmueble.setZona(rs.getString("zona"));
+                inmueble.setTipo(rs.getString("tipo"));
+                inmueble.setCondicionesContrato(rs.getString("condicionesContrato"));
+                
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el inmueble");
+                ps.close();
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Inmueble " + ex.getMessage());
+        } finally {
+            try {
+                Conexion.getConexion().close();
+            } catch (SQLException ex) {
+                Logger.getLogger(InmuebleData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return inmueble;
+    }
+    
+    public List<Inmueble> listarInmueble() {
+        List<Inmueble> inmuebles = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM inmueble";
+            ps = Conexion.getConexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Inmueble inmueble = new Inmueble();
+                     
+                inmueble.setId(rs.getInt("id"));
+                inmueble.setIdPropietario(rs.getInt("idPropietario"));
+                inmueble.setSuperficie(rs.getInt("superficie"));
+                inmueble.setCantAmbientes(rs.getInt("cantAmbientes"));
+                inmueble.setCanBaños(rs.getInt("canBaños"));
+                inmueble.setFechaConstruccion(rs.getDate("fechaConstruccion").toLocalDate());
+                inmueble.setGarage(rs.getInt("garage"));
+                inmueble.setEstadoInmueble(rs.getString("estadoInmueble"));
+                inmueble.setDireccion(rs.getString("direccion"));
+                inmueble.setZona(rs.getString("zona"));
+                inmueble.setTipo(rs.getString("tipo"));
+                inmueble.setCondicionesContrato(rs.getString("condicionesContrato"));
+                
+                inmuebles.add(inmueble);
+                
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla inmueble " + ex.getMessage());
+        } finally {
+            try {
+                Conexion.getConexion().close();
+            } catch (SQLException ex) {
+                Logger.getLogger(InmuebleData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return inmuebles;
+    }
+    
+    
     
 }
