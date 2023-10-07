21c8ebe59d47d8c81c022dbea0259fe8b7630833
LauraEtch <138030718+LauraEtch@users.noreply.github.com>
2023-10-07 11:44:32 -0300

    Update MultaData.java

--- Inmobiliaria/src/AccesoADatos/MultaData.java
+++ Inmobiliaria/src/AccesoADatos/MultaData.java
@@ -4,10 +4,55 @@
  */
 package AccesoADatos;
 
+
+import Entidades.Multa;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USUARIO
  */
 public class MultaData {
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
     
+    private MultaData(){
+    
+    }
+    
+    public void GuardarMulta(Multa multa){
+        String sql = "INSERT INTO multa (idInspeccion, idInquilino, fechaConfeccion, fechaPago, monto ) VALUES (?, ?, ?, ?, ?)";
+        try {
+            ps = Conexion.getConexion().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, multa.getIdInspeccion());
+            ps.setInt(2, multa.getIdInquilino());
+            ps.setDate(3, Date.valueOf(multa.getFechaConfeccion()));
+            ps.setDate(4, Date.valueOf(multa.getFechaPago()));
+            ps.setDouble(5, multa.getMonto());
+            ps.executeUpdate();
+            rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                multa.setId(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "La multa se generó con exito.");
+                
+            }
+        }catch(SQLException ex){
+            Logger.getLogger(MultaData.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "No se pudo generar la multa" + ex);
+        } finally{
+            try {
+                Conexion.getConexion().close();
+            } catch (SQLException ex) {
+                Logger.getLogger(MultaData.class.getName()).log(Level.SEVERE, null, ex);
+                JOptionPane.showMessageDialog(null, "error de conexion" + ex);
+            }
+        }
+    }
 }
