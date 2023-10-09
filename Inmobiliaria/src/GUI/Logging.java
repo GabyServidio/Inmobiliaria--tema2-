3ad2e4a5b48c54c8a441fe803e6f45a8c08358c6
FarielAce <f.ariel.acevedo@gmail.com>
2023-10-09 13:15:33 -0300

    CREATE GUI
    Se crearon las clases para las interfaces Graficas. necesarias para el programa

--- Inmobiliaria/src/GUI/Logging.java
+++ Inmobiliaria/src/GUI/Logging.java
@@ -10,13 +10,13 @@ import javax.swing.JOptionPane;
  *
  * @author Fernando Acevedo
  */
-public class Inicio extends javax.swing.JFrame {
+public class Logging extends javax.swing.JFrame {
 
     /**
      * Creates new form log
      */
     private UsuarioData usuarioData = new UsuarioData();
-    public Inicio() {
+    public Logging() {
         initComponents();
     }
 
@@ -217,21 +217,23 @@ public class Inicio extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Logging.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Logging.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Logging.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Logging.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Inicio().setVisible(true);
+                new Logging().setVisible(true);
             }
         });
     }
