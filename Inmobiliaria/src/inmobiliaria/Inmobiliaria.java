0c29f12e579652118fbbd76e57c233adf79c505d
Ironwall72 <134495742+Ironwall72@users.noreply.github.com>
2023-10-10 19:27:38 -0300

    update adminmuebles

--- Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
+++ Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
@@ -11,10 +11,10 @@ public class Inmobiliaria {
      */
     public static void main(String[] args) {
 
-        MultaData nuevo= new MultaData();
-        Multa nue = new Multa(15, 10, LocalDate.now(), LocalDate.now(), 10000.00);
+        //MultaData nuevo= new MultaData();
+        //Multa nue = new Multa(15, 10, LocalDate.now(), LocalDate.now(), 10000.00);
         
-        nuevo.GuardarMulta(nue);
+        //nuevo.GuardarMulta(nue);
 
     }
     
