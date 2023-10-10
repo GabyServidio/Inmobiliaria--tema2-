6683cd67058f48d991e1b14ce71dec157a916aa0
FarielAce <f.ariel.acevedo@gmail.com>
2023-10-10 09:47:34 -0300

    Inmobiliaria Update
    se llamo a la ventana de logging desde el main

--- Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
+++ Inmobiliaria/src/inmobiliaria/Inmobiliaria.java
@@ -1,8 +1,6 @@
 package inmobiliaria;
 
-import AccesoADatos.*;
-import Entidades.*;
-import java.time.LocalDate;
+import GUI.Logging;
 
 public class Inmobiliaria {
 
@@ -10,14 +8,9 @@ public class Inmobiliaria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     InspeccionData insp = new InspeccionData();
-     //Inspeccion nueva = new Inspeccion(1, 2, LocalDate.now(), "inspecion de prueba");
-     
-        for (Inspeccion inspe : insp.listarInspeciones()) {
-            System.out.println(inspe.getDescripcion());
-        }
-     
-    
+       Logging log = new Logging();
+       log.setLocationRelativeTo(null);
+       log.setVisible(true);
     }
     
 }
