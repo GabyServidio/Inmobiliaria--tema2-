975aee9b4da5ce976ec77079589f666becb6613d
GabyServidio <gabrielserv@hotmail.com.ar>
2023-10-18 12:37:00 -0300

    comprobación de campos  vacíos

--- Inmobiliaria/src/GUI/AdmInspecciones.java
+++ Inmobiliaria/src/GUI/AdmInspecciones.java
@@ -109,8 +109,14 @@ public class AdmInspecciones extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+if (jDCFecha == null){
+JOptionPane.showMessageDialog(this, "El campo fecha debe estar completo");
+}else if (jTADetalle==null || jTADetalle.equals(" ")){
+    JOptionPane.showMessageDialog(this, "El campo Detalle debe estar completo");
+}else{
+        guardar();      } 
     }//GEN-LAST:event_jButton1ActionPerformed
+    
 
     /**
      * @param args the command line arguments
